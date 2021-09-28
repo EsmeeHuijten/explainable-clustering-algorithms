4784b1d1bd9d78d0655dd11fe9a4c2250174122e
Georg Anegg <georg.anegg@gmail.com>
2021-09-28 08:47:56 +0200

    Renamed artificial to synthetic

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import algorithms.randomized
-import data.artificial
+import data.synthetic
 import visualization.clusterings
 
-instance = data.artificial.k_clusters(5)
+instance = data.synthetic.k_clusters(5)
 solver = algorithms.randomized.RandomCenters()
 output = solver.solve(instance)
 visualization.clusterings.show_clusters(output.instance.points, output.centers)
