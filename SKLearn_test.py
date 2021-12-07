3a6f6357d9319c6ddc6aa8c0e009cad3ada1353b
ehuijten <ehuijten@student.ethz.ch>
2021-12-07 08:44:34 +0100

    ExplainableOutput as output for SKLearn module

--- SKLearn_test.py
+++ SKLearn_test.py
@@ -1,11 +1,11 @@
 import algorithms.sklearn_builtins
 import data.synthetic
 import data.mall.conversion
-import visualization.clusterings
+import visualization.imm_with_precluster
 
 instance = data.synthetic.k_clusters(k=5, cluster_size=10, spread=1.5)
 #mall_parameters = {'age': False, 'income': True, 'spending_score': True}
 #instance = data.mall.conversion.get_instance(k=5, parameters=mall_parameters)
 solver = algorithms.sklearn_builtins.SKLearn()
 output = solver(instance, None)
-#visualization.clusterings.show_clusters(output)
+visualization.imm_with_precluster.show_explainable_clusters(output)
--- algorithms/sklearn_builtins.py
+++ algorithms/sklearn_builtins.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
-
+from math import inf
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.tree import DecisionTreeClassifier, plot_tree
-
+from solver_interface import ExplainableOutput, ClusterNode, make_kids
 import matplotlib.pyplot as plt
 
 from util import Point
@@ -22,9 +22,10 @@ class SKLearn:
 
         # extract pre_clusters from KMeans
         pre_centers = [Point(centercoord) for centercoord in kmeans_out.cluster_centers_]
-        pre_clusters = {center: [point for point, label in zip(kmeans_in, kmeans_out.labels_) if
+        pre_clusters = {center: [point for point, label in zip(instance.points, kmeans_out.labels_) if
                                  label == pre_centers.index(center)] for center in
                         pre_centers}
+        # TODO: warum nicht [point for point in kmeans_in if point == center]?
 
         # run DecisionTreeClassifier
         X = np.array(kmeans_in)
@@ -34,6 +35,23 @@ class SKLearn:
         plot_tree(dec_tree)
         plt.show()
 
-        # TODO: build ExplainableOutput from dec_tree
+        underlyingtree = dec_tree.tree_
+        leaves = []
+        split_nodes = []
+        dim = instance.dimension()
+        root = ClusterNode(pre_clusters, np.array([[-inf, inf]] * dim))
 
-        return None #ExplainableOutput(instance, leaves, split_nodes, pre_clusters)
+        def rec_tree(node: ClusterNode, k: int):
+            i = underlyingtree.feature[k]
+            theta = underlyingtree.threshold[k]
+
+            if underlyingtree.children_left[k] == -1:
+                leaves.append(node)
+            else:
+                split_nodes.append(node)
+                node_L, node_R = make_kids(node, i, theta, False)
+                rec_tree(node_L, underlyingtree.children_left[k])
+                rec_tree(node_R, underlyingtree.children_right[k])
+        rec_tree(root, 0)
+
+        return ExplainableOutput(instance, leaves, split_nodes, pre_clusters)
